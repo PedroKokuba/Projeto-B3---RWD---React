20683bd533676a069566cde25ecf9fbccf1f1f6a
PedroKokuba <pedro.kokuba@gmail.com>
2023-04-02 16:00:57 -0300

    Responsivo
    Acerto de detalhes

--- src/componentes/Home.js
+++ src/componentes/Home.js
@@ -6,14 +6,11 @@ import {Link} from 'react-router-dom'
 export default function Home(){
 
     return(
-        <>
-            <section>
-                <h1>Comece a entender sobre mercado financeiro</h1>
-                <p className="container__abertura__subtitulo">Iremos apresentar conceitos básicos para iniciar a sua independência financeira</p>
-                <Link className="container__abertura__botao" to="/PaginaPrincipal">Vamos lá?</Link>
-            </section>
-        
-        </>
+        <section className='pagina__inicio'>
+            <h1>Comece a entender sobre mercado financeiro</h1>
+            <p className="container__abertura__subtitulo">Iremos apresentar conceitos básicos para iniciar a sua independência financeira</p>
+            <Link className="container__abertura__botao" to="/PaginaPrincipal">Vamos lá?</Link>
+        </section>
     )
 
 
